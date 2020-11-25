3d6ad6af61326c9f67ff911c6efbed8a6691711a
NVD <roman97.46@gmail.com>
2020-11-25 22:07:48 +0100

    added client test functionality

--- foro-manglar-client/src/App.js
+++ foro-manglar-client/src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const { styled } = require("@material-ui/core/styles");
+const { Component } = require("react");
+require("./App.css");
+const { Button, Grid, Box } = require("@material-ui/core");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StyledButton = styled(Button)({
+  textTransform: "none",
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stupidButton: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Grid container spacing={1} direction="column">
+          <Grid item>
+            <StyledButton
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                this.setState({
+                  stupidButton: true,
+                });
+              }}
+            >
+              /api/test
+            </StyledButton>
+
+            {this.state.stupidButton && <LoginInfo />}
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+class LoginInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      data: undefined,
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8880/api/test")
+      .then((res) => {
+        return res.json();
+      })
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log("error :>> ", error);
+          this.setState({
+            isLoaded: true,
+            data: error,
+          });
+        }
+      );
+  }
+
+  render() {
+    if (!this.state.isLoaded) {
+      return <Box>Loading...</Box>;
+    } else {
+      return <Box> Data: {JSON.stringify(this.state.data)}</Box>;
+    }
+  }
 }
 
 export default App;
