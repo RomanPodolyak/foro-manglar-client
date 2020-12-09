67da421a8d2be3cecae4e56db0d16fad56bca225
NVD <roman97.46@gmail.com>
2020-12-09 23:37:33 +0100

    added the new components & finished first version

--- foro-manglar-client/src/App.jsx
+++ foro-manglar-client/src/App.jsx
@@ -1,31 +1,15 @@
-import React, { useState } from "react";
-import { styled } from "@material-ui/core/styles";
+import React from "react";
 import "./App.css";
-import {
-  Button,
-  Grid,
-  Typography,
-  ButtonGroup,
-  Hidden,
-} from "@material-ui/core";
-import Album from "./testComponents/Album";
+import { Grid } from "@material-ui/core";
 import Drawer from "./components/Drawer";
-import TestText from "./testComponents/TestText";
-import ListElements from "./components/ListElements";
+import ListThemes from "./components/ListThemes";
+import ListThemesPosts from "./components/ListThemesPosts";
+import ListComments from "./components/ListComments";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import RouterButton from "./components/RouterButton";
-import { Home } from "@material-ui/icons";
-import LoginInfo from "./testComponents/TestApi";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
-const StyledButton = styled(Button)({
-  textTransform: "none",
-});
-
 export default function App() {
-  const [stupidButton, setStupidButton] = useState(false);
-
   return (
     <Router>
       <Switch>
@@ -39,49 +23,15 @@ export default function App() {
           <Drawer>
             <Grid container direction="column" spacing={2}>
               <Grid item>
-                <ButtonGroup variant="contained" color="secondary">
-                  <RouterButton startIcon={<Home />} to="/">
-                    home
-                  </RouterButton>
-                  <RouterButton to="/read/themes/all">themes</RouterButton>
-                  <RouterButton to="/test-api">test api</RouterButton>
-                  <RouterButton to="/test-text">test text</RouterButton>
-                  <RouterButton to="/test-album">test album</RouterButton>
-                </ButtonGroup>
-              </Grid>
-              <Grid item>
                 <Switch>
                   <Route exact path="/">
-                    <Hidden xsUp>
-                      <Typography variant="h3">Hola, Myri :)</Typography>
-                      <Typography variant="h3" color="secondary">
-                        Te quiero &lt;3
-                      </Typography>
-                    </Hidden>
+                    <ListThemes />
                   </Route>
-                  <Route exact path="/read/themes/all">
-                    <Typography variant="h4">Temas</Typography>
-                    <ListElements
-                      url={`${process.env.REACT_APP_SERVER_API}/read/themes/all`}
-                    />
+                  <Route exact path="/themes/:themeId">
+                    <ListThemesPosts />
                   </Route>
-                  <Route exact path="/test-api">
-                    <StyledButton
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {
-                        setStupidButton(!stupidButton);
-                      }}
-                    >
-                      /api/test
-                    </StyledButton>
-                    {stupidButton && <LoginInfo />}
-                  </Route>
-                  <Route exact path="/test-text">
-                    <TestText />
-                  </Route>
-                  <Route exact path="/test-album">
-                    <Album />
+                  <Route exact path="/posts/:postId">
+                    <ListComments />
                   </Route>
                 </Switch>
               </Grid>
