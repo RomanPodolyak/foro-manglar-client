aa69fafe12f034e82fe91eba50145737a47a5e8d
November <roman97.46@gmail.com>
2022-10-05 01:19:57 +0200

    updated some code to latest version

--- foro-manglar-client/src/index.jsx
+++ foro-manglar-client/src/index.jsx
@@ -1,13 +1,15 @@
-import dotenv from "dotenv";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import dotenv from 'dotenv';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
+// Load dotenv
 dotenv.config();
 
+// Main theme
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -20,16 +22,16 @@ const theme = createMuiTheme({
   },
 });
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(); //TODO measure performance
